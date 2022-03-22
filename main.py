f7630f61963ebede3c632fd1fbdab1615ac65f19
katpomp <katpompp@gmail.com>
2022-03-22 00:38:30 -0400

    help bella

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def mommy():
+
+@app.route('/')
+def index():
     return flask.render_template('index.html')
+
+@app.route('/')
+def whyilovebella():
+    return flask.render_template('whyilovebella.html')
+
+@app.route('/')
+def aboutme():
+    return flask.render_template('aboutme.html')
 
 
 if __name__ == "__main__":
