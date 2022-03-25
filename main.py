5126639fd418abe0c62e240ef0453bdcc47f2822
katpomp <katpompp@gmail.com>
2022-03-25 11:53:17 -0400

    style

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
-import flask
-from flask import Flask
+# This is a sample Python script.
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return flask.render_template('index.html')
-
-@app.route('/links')
-def whyilovebella():
-    return flask.render_template('links.html')
-
-@app.route('/aboutme')
-def aboutme():
-    return flask.render_template('aboutme.html')
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-if __name__ == "__main__":
-    app.run()
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
