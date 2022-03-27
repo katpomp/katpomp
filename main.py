1736a05ba3e0695c0e4fc0de369309df32d1f01a
katpomp <katpompp@gmail.com>
2022-03-26 23:51:31 -0400

    hahah i hate this

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ from os import getenv
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
 
 @app.route('/links')
 def whyilovebella():
     return flask.render_template('links.html')
 
+
 @app.route('/aboutme')
 def aboutme():
     return flask.render_template('aboutme.html')
+
 
 @app.route('/myfriends')
 def myfriends():
