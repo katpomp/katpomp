46ed8d20602c48606419f33a275e40cd713edab0
katpomp <katpompp@gmail.com>
2022-03-27 00:11:35 -0400

    please work

--- main.py
+++ main.py
@@ -24,3 +24,8 @@ def aboutme():
 @app.route('/myfriends')
 def myfriends():
     return flask.render_template('myfriends.html')
+
+
+@app.route('/4russ')
+def forruss():
+    return flask.render_template('4russ.html')
